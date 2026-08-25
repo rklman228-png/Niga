54f87e45d7d4ee511051ec3135a1aa1aa5198c71
rklman228-png <rklman228@gmail.com>
2026-08-25 23:41:02 +0700

    host-control: dump deployed zone rendering methods

--- control/command.sh
+++ control/command.sh
@@ -1,3 +1,12 @@
 set -euo pipefail
 J=/opt/minecraft/server/mods/brigada-core-0.1.0.jar
-javap -classpath "$J" -p dev.brigada13.core.challenge.ChallengeService | grep -E 'tick|zone|Zone|ring|Ring|particle|Particle|hold|Hold|extraction|Extraction|split|Split|spawn|Spawn|event' | sed -n '1,240p'
+O=/tmp/csvc.javap
+javap -classpath "$J" -c -p dev.brigada13.core.challenge.ChallengeService > "$O"
+for m in tickExtraction tickSplit tickHold renderZone participantsSplit; do
+  echo "===== $m ====="
+  awk -v m="$m" '
+    $0 ~ "^[ ]+(private|public|protected).* " m "\\(" {p=1}
+    p {print}
+    p && NR>1 && $0 ~ "^[ ]+(private|public|protected).*\\(" && $0 !~ " " m "\\(" {exit}
+  ' "$O" | head -n 700
+ done
