29417feb25bc5d882f8ad32debd08e5fa41d62ba
rklman228-png <rklman228@gmail.com>
2026-08-26 03:37:07 +0700

    Expose SpringBoard hard-gate diagnostics

--- Core/SpringBoard/NigaSpringBoardService.h
+++ Core/SpringBoard/NigaSpringBoardService.h
@@ -10,6 +10,7 @@ bool niga_sbs_windowing_service_available(void);
 void niga_sbs_request_windowing_mode(int mode, NigaSpringBoardCompletion completion);
 void niga_sbs_request_reset_layout(NigaSpringBoardCompletion completion);
 char *niga_sbs_copy_diagnostics(void);
+char *niga_sbs_copy_gate_diagnostics(void);
 void niga_sbs_free_string(char *value);
 
 #endif
