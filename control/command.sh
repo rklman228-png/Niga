21c0b5f16f79c721915ef4a72005f38fca7d3acc
rklman228-png <rklman228@gmail.com>
2026-08-25 10:00:38 +0700

    Inspect prediction constants

--- control/command.sh
+++ control/command.sh
@@ -1,4 +1,3 @@
 set -euo pipefail
 J=/root/.gradle/caches/fabric-loom/minecraftMaven/net/minecraft/minecraft-merged-deobf/26.3-snapshot-9/minecraft-merged-deobf-26.3-snapshot-9.jar
-javap -classpath "$J" -p net.minecraft.world.entity.player.Inventory
-jar tf "$J" | grep '/Prediction.class$' | head -30
+javap -classpath "$J" -p net.minecraft.util.Prediction
