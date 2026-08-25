2e118bc694b26c696511147eb9e2eab230dbcd95
rklman228-png <rklman228@gmail.com>
2026-08-26 03:05:00 +0700

    Add direct SpringBoard windowing service bridge

---
+++ Core/SpringBoard/NigaSpringBoardService.h
@@ -0,0 +1,15 @@
+#ifndef NIGA_SPRINGBOARD_SERVICE_H
+#define NIGA_SPRINGBOARD_SERVICE_H
+
+#import <Foundation/Foundation.h>
+#include <stdbool.h>
+
+typedef void (^NigaSpringBoardCompletion)(bool completed);
+
+bool niga_sbs_windowing_service_available(void);
+void niga_sbs_request_windowing_mode(int mode, NigaSpringBoardCompletion completion);
+void niga_sbs_request_reset_layout(NigaSpringBoardCompletion completion);
+char *niga_sbs_copy_diagnostics(void);
+void niga_sbs_free_string(char *value);
+
+#endif
