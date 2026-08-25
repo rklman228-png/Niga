1edeccff60c87c1de426e47d2339da06287b73e5
rklman228-png <rklman228@gmail.com>
2026-08-26 01:35:10 +0700

    Add FrontBoard scene probe bridge header

---
+++ Core/Scene/NigaSceneProbe.h
@@ -0,0 +1,15 @@
+#ifndef NIGA_SCENE_PROBE_H
+#define NIGA_SCENE_PROBE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char *niga_scene_probe_json(const char *bundle_id);
+void niga_scene_probe_free(char *value);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
